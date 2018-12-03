14d65e64c5454a9b3d82caead728fafd6d286fc0
homeschneiju <45554489+homeschneiju@users.noreply.github.com>
2018-12-03 17:38:43 +0100

    Revert "v02: stelle fest, dass es nicht funktioniert"
    This reverts commit f7f781755ba9be9c281669049942cb513812f0f9.

--- attempt cpp in r.R
+++ attempt cpp in r.R
@@ -5,8 +5,6 @@ library(XML)
   library(devtools)
 library(WikipediR)
 
-
-
 # devtools::install_github("ironholds/WikipediR")
 #Retrieve the categories for the "New Age" article on en.wiki
 cats <- categories_in_page("en", "wikipedia", pages = "New Age")
@@ -18,4 +16,3 @@ top_editors_page <- "http://en.wikipedia.org/wiki/Wikipedia:List_of_Wikipedians_
 
 top_editors_table <- readHTMLTable(top_editors_page)
 very_top_editors <- as.character(top_editors_table[[3]][1:5,]$User)
-#funktioniert nicht
